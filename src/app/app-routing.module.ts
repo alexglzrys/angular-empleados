09d7220df0f97c36ed47e4997eccbab70cb39469
Alejandro González Reyes <alexglzrys@gmail.com>
2022-11-16 16:56:25 -0600

    configurar rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaEmpleadosComponent } from './components/lista-empleados/lista-empleados.component';
+import { CrearEmpleadoComponent } from './components/crear-empleado/crear-empleado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListaEmpleadosComponent, pathMatch: 'full' },
+  { path: 'create', component: CrearEmpleadoComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
